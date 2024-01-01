8ed18a39cfed8d35a1f476c663705e3310171b37
Douglas <douglasirrazabal@gmail.com>
2023-12-31 22:22:06 -0300

    refactor: minor cleanup

--- src/repositories/characterRepositoryAPI.ts
+++ src/repositories/characterRepositoryAPI.ts
@@ -5,23 +5,6 @@ import axios from "axios";
 const BASE_URL_API = 'https://rickandmortyapi.com/api/character';
 
 export const CharacterRepositoryAPI = {
-  fetchCharacters: async () => {
-    try {
-      const response = await axios.get<CharacterResponse>(`${BASE_URL_API}`);
-      const characters = response.data.results;
-      const charactersAdapted: Character[] = characters.map((data) => ({
-        id: data.id,
-        name: data.name,
-        status: data.status,
-        gender: data.gender,
-        image: data.image
-      }));
-  
-      return charactersAdapted;
-    } catch (error) {
-      throw new Error('Failed to fetch characters from API');
-    }
-  },
   searchCharacters: async (name: string) => {
     try {
       const response = await axios.get<CharacterResponse>(`${BASE_URL_API}?name=${name}`);
